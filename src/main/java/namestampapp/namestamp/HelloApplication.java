75acce05845d966cf508f136d21633344d9e7cd6
Ricardo Meireles <ricardomanuel.meireles@gmail.com>
2025-04-02 17:09:46 +0100

    Cleanup (not to use JavaFX in this moment).

--- src/main/java/namestampapp/namestamp/HelloApplication.java
+++ src/main/java/namestampapp/namestamp/HelloApplication.java
@@ -1,13 +1,9 @@
 package namestampapp.namestamp;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
+public class HelloApplication {
+/*
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -16,8 +12,9 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+     */
 
     public static void main(String[] args) {
-        launch();
+        //launch();
     }
 }
